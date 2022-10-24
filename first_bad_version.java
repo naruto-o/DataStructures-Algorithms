034cc14a8352804d3454c680c08529196fe54087
naruto <73755581+naruto-o@users.noreply.github.com>
2022-10-24 15:17:14 +0530

    Update first_bad_version.java

--- first_bad_version.java
+++ first_bad_version.java
@@ -7,7 +7,7 @@ public class first_bad_version {
         int hi = n;
         int ans = 0;
         while(lo <= hi){
-            int mid = (lo + hi)/2;
+            int mid = lo +( hi - lo)/2;
             if(isBadVersion(mid) == true){
                 ans  = mid;
                 hi = mid -1;
