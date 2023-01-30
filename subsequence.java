3e9b9ccfc3ba237933a081428a49c487b8145fd3
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2023-01-30 23:36:47 +0530

    Add files via upload

---
+++ subsequence.java
@@ -0,0 +1,15 @@
+public class subsequence {
+    public static void main(String[] args) {
+        String str = "abc";
+        printSubSeq(str,"");
+    }
+    public static void printSubSeq(String ques,String ans){
+        if(ques.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        char ch = ques.charAt(0);
+        printSubSeq(ques.substring(1), ans);
+        printSubSeq(ques.substring(1), ans+ch);
+    }
+}
